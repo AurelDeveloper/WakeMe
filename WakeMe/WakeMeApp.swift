7b7d3f1bd75935ffa9d812ccbfcfc5f74a279342
AurelDeveloper <marlon.profils@gmail.com>
2023-12-18 15:11:13 +0100

    Initial Commit

---
+++ WakeMe/WakeMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeMeApp.swift
+//  WakeMe
+//
+//  Created by Marlon Hinoran on 18.12.23.
+//
+
+import SwiftUI
+
+@main
+struct WakeMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
