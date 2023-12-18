2e212ff8897c46ebd2fe1efe5a174e3f9a08cfda
AurelDeveloper <marlon.profils@gmail.com>
2023-12-18 18:59:37 +0100

    first commit

---
+++ WakeMe/AlarmDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  AlarmDetailsView.swift
+//  WakeMe
+//
+//  Created by Marlon Hinoran on 18.12.23.
+//
+
+import SwiftUI
+
+struct AlarmDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlarmDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlarmDetailsView()
+    }
+}
